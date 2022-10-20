38bd0d2251d24468de58ec74248cdd6731fcdce7
isaac <0012126@student.isf.edu.hk>
2022-10-20 11:29:53 +0800

    hello

--- all_riddles.py
+++ all_riddles.py
@@ -1,7 +1,7 @@
 riddles_json = {"riddles":
     [
     {"correct":0,"guesses":0,"id":89,"question":"1"},
-    {"correct":529,"guesses":549,"id":32,"question":"Who's named after a city but as tall as the eiffel tower?"},
+    {"correct":529,"guesses":549,"id":32,:"Who's named after a city but as tall as the eiffel tower?"},
     {"correct":87,"guesses":118,"id":5,"question":"I posted a riddle, who am i?"},
     {"correct":1,"guesses":2,"id":88,"question":"Ocean"},
     {"correct":14,"guesses":23,"id":61,"question":"You start at red, but stop at green. What am I?"},
